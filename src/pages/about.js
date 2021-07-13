cf8745b212f98963d27d7f98c6254b6761f504cc
Waylon Baumgardner <waylonrobert@gmail.com>
2021-07-13 16:48:33 -0700

    More tweaks. Fixes #11, fixes #12

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
@@ -15,46 +16,20 @@ export default function About() {
         />
 
         <div className="mx-auto w-11/12 md:w-3/4 my-8">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto w-11/12 md:w-full my-8">
-            <div>
-              <h2 className="uppercase font-worksans text-heading-text font-semibold text-xs md:text-sm">
-                My interests
-              </h2>
-              <ul className="about-interests dark:text-dark-text">
-                <li>Writing</li>
-                <li>
-                  <Link to="/reading-log">Reading</Link>
-                </li>
-                <li>
-                  <Link to="/music-log">Music</Link>
-                </li>
-                <li>Web design/dev</li>
-                <li>Obscure 90's TV</li>
-                <li>British TV</li>
-                <li>Coffee</li>
-                <li>Bourbon</li>
-                <li>Cars</li>
-                <li>Console video games</li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="uppercase font-worksans text-heading-text font-semibold text-xs md:text-sm text-left">
-                My knowledge areas
-              </h2>
-
-              <ul className="about-interests dark:text-dark-text">
-                <li>Higher education</li>
-                <li>Leadership</li>
-                <li>Nonprofits</li>
-                <li>Systems thinking</li>
-                <li>User-centered design</li>
-                <li>Computers &amp; technology</li>
-                <li>Web development</li>
-                <li>Writing</li>
-              </ul>
-            </div>
+          <div className="float-left pr-4">
+            <StaticImage
+              src="../images/waylon-baumgardner-portrait.jpg"
+              alt="Waylon Baumgardner"
+              width={250}
+            />
           </div>
-
+          <p className="mb-4">
+            Hi, my name is Waylon Baumgardner, and I'm a UX designer, front-end
+            developer who knows enough to be dangerous, and <em>amateur</em>{' '}
+            violinist (<em>very</em> amateur) from and based out of Southern
+            California. I grew up in San Diego, and now reside somewhere in
+            between San Diego and LA.
+          </p>
           <p>
             I started my professional career as a web developer/designer. I
             spent ten years in higher education where I held various web
@@ -62,10 +37,10 @@ export default function About() {
             career entirely, pursuing my love of UX design by joining Esri as a
             Senior UX Designer.{' '}
           </p>
-          <h2 className="my-8">What I Believe</h2>
+          <h2 className="mt-14 mb-8">What I Believe</h2>
           <p>
             I've documented some truths I personally believe in that shape and
-            influence how I work.
+            influence how I work:
           </p>
           <ul className="mt-4 dark:text-dark-text">
             <li>
@@ -86,10 +61,52 @@ export default function About() {
               Users <em>sometimes</em> need guidance
             </li>
           </ul>
-          <h2 className="my-8">What People Say About Me</h2>
         </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto w-11/12 md:w-3/4 my-8">
+          <div>
+            <h2 className="uppercase font-worksans text-heading-text font-semibold text-xs md:text-sm">
+              My interests
+            </h2>
+            <ul className="about-interests dark:text-dark-text">
+              <li>Writing</li>
+              <li>
+                <Link to="/reading-log">Reading</Link>
+              </li>
+              <li>
+                <Link to="/music-log">Music</Link>
+              </li>
+              <li>Web design/dev</li>
+              <li>Obscure 90's TV</li>
+              <li>British TV</li>
+              <li>Coffee</li>
+              <li>Bourbon</li>
+              <li>Cars</li>
+              <li>Console video games</li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="uppercase font-worksans text-heading-text font-semibold text-xs md:text-sm text-left">
+              My knowledge areas
+            </h2>
+
+            <ul className="about-interests dark:text-dark-text">
+              <li>Higher education</li>
+              <li>Leadership</li>
+              <li>Nonprofits</li>
+              <li>Systems thinking</li>
+              <li>User-centered design</li>
+              <li>Computers &amp; technology</li>
+              <li>Web development</li>
+              <li>Writing</li>
+            </ul>
+          </div>
+        </div>
+        <h2 className="my-8 mx-auto w-11/12 md:w-3/4">
+          What People Say About Me
+        </h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-4 md:gap-16 mx-auto w-11/12">
+
+      <div className="grid grid-cols-1 md:grid-cols-4 md:gap-16 mx-auto w-3/4 md:w-11/12">
         <div>
           <blockquote>
             I really appreciated Waylon's help in creating my Arousal
